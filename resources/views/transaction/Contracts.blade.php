cfa2eedf89c624fa5f489bbd77b6dd88af9951c0
orangetwinspur <vivascoleen@yahoo.com>
2017-07-14 01:10:43 +0800

    Updated Contract View

--- resources/views/transaction/Contracts.blade.php
+++ resources/views/transaction/Contracts.blade.php
@@ -1,38 +1,40 @@
 @extends('layout.app')
 @section('content-header')
 
-<h1>List of Tenants</h1>
+<h1>List of Contracts</h1>
   @stop
 
   @section('content')
-  	  <div class="box">
+      <div class="box box-primary">
             <div class="box-header">
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-              <table id="example1" class="table table-bordered table-striped">
+           
+            <div class = "table-responsive">
+              <table id="tblcontract" class="table table-bordered table-striped" width="100%">
+              
                 <thead>
                 <tr>
+                  <th>No</th>
+                  <th>Stall ID</th>
                   <th>Name</th>
-                  <th>Current Active Contract</th>
-                  <th>Action</th>
+                  <th>Starting Date</th>
+                  <th>Contract Ends</th>
+                  <th>Status</th>
+                  <th>Actions</th>
                 </tr>
                 </thead>
                 <tbody>
-           			<tr>
-           				<td>Coleen Vivas</td>
-           				<td>2</td>
-           				<td>
-           					<a href="/ViewContracts" ><button class ="btn btn-success" align="center">View</button></a>
-           				</td>
-           			</tr>
+             
                 </tbody>
+                
               </table>
             </div>
             <!-- /.box-body -->
           </div>
           <!-- /.box -->
-
+          </div>
           <!-- MODAL -->
           <!-- Modal -->
 <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
@@ -52,14 +54,143 @@
     </div>
   </div>
 </div>
+
+<!--/.modal new contract=-->
+<!--modal view-->
+ <div class="modal fade" tabindex="-1" id="new" role="dialog">
+                    <div class="modal-dialog modal-lg" role="document">
+                        
+                            <input type="hidden" name="_token" value="<{{ csrf_token() }}">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title">New Contract</h4> </div>
+                                <div class="modal-body">
+                                    <div class = "col-md-12 form-group row">
+                                        <div class = "col-md-12">
+                                        <label><b>Stall Holder Name:</b></label>
+                                    
+                                        <input type = "text" class = "form-control" id = "searchbox" name = "searchbox" value = "" placeholder="Search..."  />
+                                      </div>
+
+                                      
+                            
+                                     </div>
+                                     <form action="" method="post" id="newform">
+                            <div class = "col-md-12 form-group row">
+                            <div class = "col-md-12">
+                            <label for = "org"><b>Name of Group/Organization</b></label>
+                            <label for ="ven_orgname" class = "form-control" ></label>
+                        </div>
+                        </div>
+                  <div class="col-md-12 form-group row">
+
+                    <div class="col-md-12">
+
+                      <label for="sh_name"><b>Stall Holder Name:</b></label>
+                    
+                      <label for = "vendor_name" id="ven_name" ></label>
+                    </div>
+                  </div>
+                  
+                  
+               
+
+                               
+                                 <div class = "col-md-12 form-group row">
+                                <div class = "col-md-6">
+                                        <label for = "email">*Email Address:</label>
+                                        <input type = "email" class = "form-control" id = "email" name = "email"/>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <label for="phone"><b>* Mobile:</b></label>
+                                    <input type="text" class="form-control" id="mob" name="mob"required>
+                                 </div>
+                                 </div>
+
+
+                           
+                            <div class="col-md-12 form-group row">
+                                <div class = "col-md-12">
+                                    <label for="address"><b>*Home Address:</b></label>
+                            
+                                    <textarea rows="4" class="form-control" id="address" name="address"></textarea>
+                                </div>
+                            </div>
+                            
+                                
+                                </div>
+                                <div class="modal-footer">
+                                    
+                                    <button class="btn btn-info pull-right" style="background-color:#191966" id = "btn-submit">Submit</button>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+                <!--end of modal view-->
   @stop
 
   @section('script')
-  	<script>
-	  $(function () {
-	    $("#example1").DataTable();
-	    
-	  });
+    <script>
+      $(document).ready(function () {
+            $('#tblcontract').DataTable({
+                ajax: '/rentInfo'
+            , responsive: true
+            , "columns": [
+                 {
+                    "data": "rentID"
+                    }
+                    , {
+                    "data": "stallID" }
+                    , {
+                   "data" : function(data, type, dataToSet){
+                            return (data.venFName+
+                              " "+data.venLName);
+                        }
+                    }
+                    , { "data" :  function(data, type, dataToSet){
+                      var options = {year: 'numeric', month: 'long', day: 'numeric' };
+                            $date = new Date(data.rentStartDate).toLocaleDateString("en-US",options);
+                        
+                            return $date;
+                        }
+                        }, //convert to MMMM-DD-YYYY
+                      { "data": "rentStartDate"},
+                      {"data": "rentStartDate"},
+                 
+                    { "data": "actions"}
+
+            ]
+            , "columnDefs": [
+                {
+                    "width": "20%"
+                    , "searchable": false
+                    , "sortable": false
+                    , "targets": 6
+                    }
+  ]
+            });
+
+      $('#newform').attr('disabled',true);
+          });
+      function printpdf(id)
+      {
+          $.ajax({
+            type:"POST",
+            url: '/printPdf',
+            data: {
+              "_token" : "{{ csrf_token() }}",
+              "id" : id
+            },
+            success : function (){
+               window.open( '_blank');
+            }
+          })
+
+      }
 </script>
 
   @stop
